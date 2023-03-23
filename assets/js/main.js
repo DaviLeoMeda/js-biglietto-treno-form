98f7e458e166124332b3e7a4eb0d723740671034
DaviLeoMeda <davileo.meda@gmail.com>
2023-03-23 22:14:29 +0100

    trying

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,29 +2,54 @@
 
 function erogazioneBiglietti() {
 
+    let nomination = document.getElementById("namesurname").value;
     let kilometraggio = document.getElementById("kilometraggio").value;
     let eta = document.getElementById("eta").value;
 
-    console.log(kilometraggio, eta);
+    console.log(nomination, kilometraggio, eta);
 
+    let price = (kilometraggio * 0.21);
 
-    if (eta > 17 && eta < 65) {
-
-        let normalprice = (kilometraggio * 0.21);
-        let roundedprice = normalprice.toFixed(2);
-        document.getElementById("trainprice").innerHTML = roundedprice;
-    }
-    else if (eta < 18) {
-        let normalprice = ((kilometraggio * 0.21) * 0.8);
-        let roundedprice = normalprice.toFixed(2);
-        document.getElementById("trainprice").innerHTML = roundedprice;
+    if (eta < 18) {
+        price *= 0.8;
     }
 
     else if (eta > 65) {
-        let normalprice = ((kilometraggio * 0.21) * 0.6);
-        let roundedprice = normalprice.toFixed(2);
-        document.getElementById("trainprice").innerHTML = roundedprice;
+        price *= 0.6;
+
 
     }
 
+
+
+    // let standardprice = (kilometraggio * 0.21);
+
+    // if (eta < 18) {
+    //     let standardprice *= 0.8
+
+
+    // if (eta > 17 && eta < 65) {
+
+    //     let normalprice = (kilometraggio * 0.21);
+    //     let roundedprice = normalprice.toFixed(2);
+    //     document.getElementById("trainprice").innerHTML = roundedprice;
+    // }
+    // else if (eta < 18) {
+    //     let normalprice = ((kilometraggio * 0.21) * 0.8);
+    //     let roundedprice = normalprice.toFixed(2);
+    //     document.getElementById("trainprice").innerHTML = roundedprice;
+    // }
+
+    // else if (eta > 65) {
+    //     let normalprice = ((kilometraggio * 0.21) * 0.6);
+    //     let roundedprice = normalprice.toFixed(2);
+    //     document.getElementById("trainprice").innerHTML = roundedprice;
+
+    // }
+
 }
+
+console.log(nomination, price);
+
+document.getElementById("nome").innerHTML = `<p>Benvenuta/o ${nomination}</p>`;
+document.getElementById("price").innerHTML = `<p>Il prezzo del biglietto è di ${price.toFixed(2)} €</p>`;
